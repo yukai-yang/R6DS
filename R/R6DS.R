a4885f6c687b21d087be4ba24747fc4b3b4d6c65
凱(Kai) <yukai.yang@gmail.com>
2019-05-09 12:21:45 +0200

    Add files via upload

---
+++ R/R6DS.R
@@ -0,0 +1,47 @@
+################################################################################
+## package name: R6DS
+## author: Yukai Yang
+## Statistiska Inst., Uppsala Universitet
+## May 2019
+#################################################################################
+
+
+#' R6DS: a package implementing some useful data stuctures by using the R6 reference class.
+#'
+#' Write something here
+#'
+#' And here.
+#'
+#'
+#'
+#' @section Author and Maintainer:
+#' Yukai Yang
+#'
+#' Department of Statistics, Uppsala University
+#'
+#' \email{yukai.yang@@statistik.uu.se}
+#'
+#' @section Linked List:
+#' \code{\link{SimModel1}} simulate from the type one state-space model on the Stiefel manifold.
+#'
+#' @section Stack:
+#' \code{\link{FilterModel1}} filtering algorithm for the type one model.
+#'
+#' @section Queue:
+#' \code{\link{runif_sm}} sample from the uniform distribution on the Stiefel manifold.
+#'
+#' @section Set:
+#' \code{\link{version}} shows the version number and some information of the package.
+#'
+#' @section Dictionary:
+#' \code{\link{version}} shows the version number and some information of the package.
+#'
+#' @section Binary Search Tree (BST):
+#' \code{\link{version}} shows the version number and some information of the package.
+#'
+#' @docType package
+#' @name R6DS
+NULL
+
+#' @importFrom R6 R6Class
+NULL
